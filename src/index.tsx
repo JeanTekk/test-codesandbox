7d6b320068652cb59307935d7cbc028b805c1d8b
JeanTekk <36802714+JeanTekk@users.noreply.github.com>
2019-09-26 23:22:56 +0200

    installed emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { css } from '@emotion/core';
+import { css } from "@emotion/core";
 import "./styles.css";
 
+const style = css(`color: red; font-size: 2em;`);
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={style}>
+      for real?
       <h1>Hello CodeSandbox</h1>
+      <h2>test</h2>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
